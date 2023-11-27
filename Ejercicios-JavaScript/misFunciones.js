362087172c405041382fac0a525837d0011edd19
DOUGLAS REZZONICO BENJAMÍN <82091184+benjadouglas@users.noreply.github.com>
2023-11-27 11:59:27 -0300

    ejercicios canvas resueltos

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -85,3 +85,57 @@ function cargarResultado(){
 
 	document.getElementById("dist").value = can + " " + un;
 }
+
+function dibujarCirCuad(){
+	var canvas = document.getElementById("myCanvas")
+	var ctx = canvas.getContext("2d")
+
+	var xMax = canvas.width;
+	var yMax = canvas.height;
+	var margen = 5;
+	ctx.fillStyle = "#333899";
+	ctx.fillRect(0+margen,yMax-40-margen,40,40)
+
+	ctx.arc(xMax/2, yMax/2, 20, 0, 2*Math.PI)
+	ctx.stroke();
+	ctx.fillStyle = "#8b4c99";
+	ctx.fill();
+}
+
+function dibujarCuadriculado(){
+	var canvas = document.getElementById("myCanvas");
+	var ctx = canvas.getContext("2d");
+
+	var alturaMax = canvas.height;
+	var anchoMax = canvas.width;
+	for (var i=0; i<alturaMax;) {
+		ctx.moveTo(0, i);
+		ctx.lineTo(1000, i);
+		ctx.strokeStyle = "#3e67d9";
+		ctx.stroke();
+		i = i + 20;
+	}
+
+	for (var i = 0; i < anchoMax;){
+		ctx.moveTo(i,0);
+		ctx.lineTo(i,1000);
+		ctx.strokeStyle = "#3e67d9"
+		ctx.stroke();
+		i = i + 20;
+	}
+}
+
+function dibujarImagen(posX, posY){
+	var canvas = document.getElementById("myCanvas");
+	var ctx = canvas.getContext("2d");
+
+	console.log(posX, posY);
+	var img = new Image();
+	img.src = "images/auto.png";
+
+	canvas.width = canvas.width;
+
+	img.onload = function (){
+		ctx.drawImage(img, posX, posY);
+	}
+}
